572dd6437f68dba4f135fb404f1b730e70b25f2f
Xy <1718077714@qq.com>
2021-11-11 21:02:19 +0800

    second commit

--- src/main/java/com/minzu/testt/git.java
+++ src/main/java/com/minzu/testt/git.java
@@ -3,6 +3,7 @@ package com.minzu.testt;
 public class git {
     public static void main(String[] args) {
         System.out.println("helloGit");
+        System.out.println("helloGit2222222");
 
 
     }
